95968569d0f2c708c31ebd67cafdcef137f48931
l_hernandez <l_hernandez@fanshaweonline.ca>
2023-12-08 20:38:57 -0500

    added about me and footer
    added a few gsap for the sections. added about me animation in text and image

--- js/main.js
+++ js/main.js
@@ -65,26 +65,93 @@ videoControls.addEventListener('mouseleave', hideControls);
 player.addEventListener('mouseenter', showControls);
 player.addEventListener('mouseleave', hideControls);
 
-//image 3d hover effect gsap
-// document.addEventListener("mousemove", function(event) {
-//     var width = window.innerWidth,
-//     height = window.innerHeight,
-//     positionX = (event.clientX/width) - 0.6,
-//     positionY = (event.clientY/height) - 0.6;
-//     gsap.to(".image_con img", {
+// image 3d hover effect gsap
+document.addEventListener("mousemove", function(event) {
+    var width = window.innerWidth,
+    height = window.innerHeight,
+    positionX = (event.clientX/width) - 0.6,
+    positionY = (event.clientY/height) - 0.6;
+    gsap.to(".image_con img", {
 
-//         rotationY: positionX * 50,
-//         rotationX: -positionY * 50,
-//         ease: "none"
+        rotationY: positionX * 50,
+        rotationX: -positionY * 50,
+        ease: "none"
 
-//     });
+    });
     
-// })
+})
 
-// // bouncing ball button
-// let link = document.querySelector(".link");
-// let ball = document.querySelector(".ball");
-// let hoverTL = gsap.timeline();
-// hoverTL.to(ball, {width: "calc(100% + 1.3em}", ease: "Elastic.easeOut(0.25)",
-// duration: 2})
-// hoverTL.to(ball, {width: "2em", left: "calc(100% +1.5em}", ease: "Elastic.easeOut(0.25)"})
+// bouncing ball button
+
+let links = document.querySelectorAll(".link");
+
+links.forEach(link => {
+    let ball = link.querySelector(".ball");
+    let hoverTL = gsap.timeline();
+    hoverTL.pause();
+
+    hoverTL.to(ball, { width: "calc(100% + 1.3em)", ease: "Elastic.easeOut(0.25)", duration: 0.5 });
+    hoverTL.to(ball, { width: "2em", left: "calc(100% - 1.45em)", ease: "Elastic.easeOut(0.25)", duration: 0.5 });
+
+    link.addEventListener("mouseenter", () => {
+        hoverTL.play();
+    });
+
+    link.addEventListener("mouseleave", () => {
+        hoverTL.reverse();
+    });
+});
+
+
+gsap.registerPlugin(ScrollTrigger)
+// load animation for headers
+
+
+// Animation for #profile section on page load
+const profileSection = document.querySelector('#profile');
+const profileElements = profileSection.querySelectorAll('.grid-con > *');
+
+gsap.from(profileElements, {
+    x: '100%', // Start from the right
+    opacity: 0,
+    stagger: 0.2, // Adjust the stagger value according to your preference
+    duration: 1, // Animation duration
+    ease: 'power2.out', // Easing function
+});
+
+// scroll trigger for text
+const splitTypes = document.querySelectorAll('.text-matrix')
+
+splitTypes.forEach((char,i) => {
+
+
+    const text = new SplitType(char, { types: 'chars'})
+
+    gsap.from(text.chars, {
+        scrollTrigger: {
+            trigger: char,
+            start: 'top 80%',
+            end: 'top 60%',
+            scrub: true,
+            markers: false,
+        },
+        y: 100,
+        opacity: 0,
+        stagger: 0.05,
+
+    })
+})
+
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+console.log(e)
+})
+
+function raf(time) {
+lenis.raf(time)
+requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
