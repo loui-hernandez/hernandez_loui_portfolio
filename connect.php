42537a80ed92040ee51c13fc3ed48718f46c5eb9
Loui Hernandez <l_hernandez@fanshaweonline.ca>
2024-02-26 09:24:38 -0500

    updating
    testing if it works

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=portfolio;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=jsok8821_lhernandez;charset=utf8mb4";
 try {
-$connection = new PDO($dsn, 'root', 'root');
+  $connection = new PDO($dsn, 'jsok8821_general', 'mTK{cdwIi-v+');
 } catch (Exception $e) {
   error_log($e->getMessage());
   exit('unable to connect');
